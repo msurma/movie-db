68925107699a510f4d4690477ab6841b6ae52f78
Michał Surmaczewski <github@msurma.net>
2024-06-21 01:09:39 +0200

    Implement MoviesStartingWithWAndEvenLength algorithm

---
+++ src/Movie/Recommender/Algorithm/MoviesStartingWithWAndEvenLength.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Movie\Recommender\Algorithm;
+
+/**
+ * Returns movies whose titles start with the letter 'W' and have an even
+ * number of characters in the title.
+ */
+final class MoviesStartingWithWAndEvenLength implements AlgorithmInterface
+{
+    public const NAME = 'movieStartingWithWAndEvenLength';
+
+    public static function getAlgorithmName(): string
+    {
+        return self::NAME;
+    }
+
+    public function getRecommendations(array $movies): array
+    {
+        return array_filter($movies, function($movie) {
+            return stripos($movie, 'W') === 0 && strlen($movie) % 2 == 0;
+        });
+    }
+}
